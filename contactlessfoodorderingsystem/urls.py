5e2eae930733212034dc01d4cecb8ca109f921ff
Malapula Narendra <141564331+narerndra369@users.noreply.github.com>
2025-04-05 23:30:45 +0530

    Update urls.py

--- contactlessfoodorderingsystem/urls.py
+++ contactlessfoodorderingsystem/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
-
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Restaurant.urls')),
@@ -23,3 +24,6 @@ urlpatterns = [
     path('',include('adminapp.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
